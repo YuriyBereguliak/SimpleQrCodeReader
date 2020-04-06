dc0472585e950de9a152c77f82266e7c6e6e6d09
Yuriy <ybereguliak95@gmail.com>
2020-04-06 16:50:13 +0300

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:qr_reader/page/HomeScreen.dart';
-import 'package:qr_reader/page/QrCodeDetailsScreen.dart';
-import 'package:qr_reader/page/QrCodeReaderScreen.dart';
-import 'package:qr_reader/page/SplashScreen.dart';
 import 'package:qr_reader/utils/constants.dart';
 
+import 'page/HomeScreen.dart';
+import 'page/QrCodeDetailsScreen.dart';
+import 'page/QrCodeReaderScreen.dart';
+import 'page/SplashScreen.dart';
 import 'utils/Routes.dart' as route;
 
 void main() => runApp(MyApp());
