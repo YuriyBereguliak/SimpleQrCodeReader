30e49c975e11fa1d9e12f59b79ce7b68fc820314
Yuriy Bereguliak <yuriy.bereguliak@inoxoft.com>
2020-04-28 17:13:03 +0300

    ListView builder start

---
+++ lib/data/model/Code.dart
@@ -0,0 +1,5 @@
+class CodeModel {
+  int type;
+  String value;
+  int time;
+}
--- lib/page/ScannedCodesScreen.dart
+++ lib/page/ScannedCodesScreen.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ScannedCodesScreen extends StatelessWidget {
+class ScannedCodesScreen extends StatefulWidget {
+  //region StatefulWidget
+  @override
+  State createState() {
+    return _ScannedCodesState();
+  }
+//endregion
+}
+
+class _ScannedCodesState extends State<ScannedCodesScreen> {
+  final List<String> _items = ["1", "2", "3", "4"];
+
+  //region State
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("List of scanned barcodes"),
+      child: ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _buildListItem(index);
+        },
+      ),
     );
   }
+
+  //endregion
+
+  //region Utility API
+  Widget _buildListItem(int index) {
+    return Container(
+      color: Colors.white,
+      child: Text(_items.elementAt(index)),
+    );
+  }
+//endregion
 }
