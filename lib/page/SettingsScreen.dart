d45b5bc7e188cf41e7f24bde5877e88d4946be90
Yuriy Bereguliak <ybereguliak95@gmail.com>
2020-04-30 18:14:41 +0300

    #5: added switch widget

--- lib/page/SettingsScreen.dart
+++ lib/page/SettingsScreen.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  @override
+  State createState() {
+    return _SettingsState();
+  }
+}
+
+class _SettingsState extends State<SettingsScreen> {
+  bool _isStartScanner = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Settings"),
+        title: const  Text("Settings"),
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text("Launch settings"),
+           Text("Launch settings", style: Theme.of(context).textTheme.title),
+            SwitchListTile(
+                title: Text("Start from scan code"),
+                value: _isStartScanner,
+                contentPadding: EdgeInsets.all(0),
+                onChanged: (bool value) {
+                  setState(() {
+                    _isStartScanner = value;
+                    print("Result :: $_isStartScanner");
+                  });
+                })
           ],
         ),
       ),
